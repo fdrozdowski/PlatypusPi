cae6b4f0e9a888e7cb7059785237d5721a9883e1
fdrozdowski <fgd1@rice.edu>
2016-01-24 22:43:40 +0000

    Changed speech synthesizer eSpeak to Ivona.

--- piteddy.py
+++ piteddy.py
@@ -1,6 +1,5 @@
 from TwitterAPI import TwitterAPI, TwitterOAuth
-from espeak import espeak
-import codecs
+import pyvona
 import os
 
 def init_twitter_api():
@@ -10,17 +9,35 @@ def init_twitter_api():
                  o.access_token_key,
                  o.access_token_secret)  
 
+   
   return api
+
+def init_ivona():
+  creds = read_credentials('ivona_credentials.txt')
+  v = pyvona.create_voice(creds['access_key'], creds['secret_key'])
+  return v
+
+def read_credentials(filename):
+  if filename is None:
+     path = os.path.dirname(__file__)
+     filename = os.path.join(path, 'ivona_credentials.txt')
+ 
+  with open(filename) as f:
+    creds = {}
+    for line in f:
+      if '=' in line:
+	name, value = line.split('=', 1)
+    	creds[name.strip()] = value.strip()
+    return creds	
 
 
 def search_last_tweets(query, tweets_number): 
   for item in api.request('search/tweets', {'q': query, 'count': tweets_number}):
     #print(item['text'] if 'text' in item else item)    
     content = item['text']
-    content = content.encode('ascii', 'ignore')
-    #content = content.replace('...', 'dot dot dot')
-    print(content)	   
-    espeak.synth(content)
+    print(content)
+    ivona.speak(content)	   
 
 api = init_twitter_api()
+ivona = init_ivona()
 search_last_tweets('Donald Trump', 5)
